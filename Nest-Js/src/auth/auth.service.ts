acee213c463408675b0eadce24f79025378e03c5
pratikgirikonvergedotai <pratik.giri@konverge.ai>
2022-03-22 23:56:14 +0530

    nest-js

---
+++ Nest-Js/src/auth/auth.service.ts
@@ -0,0 +1,43 @@
+import { ForbiddenException, Injectable } from "@nestjs/common";
+import { PrismaService } from "src/prisma/prisma.service";
+import { AuthDto } from "./dto";
+import * as argon from 'argon2';
+import { PrismaClientKnownRequestError } from "@prisma/client/runtime";
+
+
+@Injectable()
+export class AuthService{
+    constructor(private prisma: PrismaService) {}
+    async signup(dto: AuthDto){
+        const hash = await argon.hash(dto.password)
+        try{
+            const user = await this.prisma.user.create({
+                data : {
+                    email : dto.email,
+                    hash
+                }
+            })
+            delete user.hash
+            return user
+        }
+        catch(err){
+            if (err instanceof PrismaClientKnownRequestError){
+                if (err.code === 'P2002'){
+                    throw new ForbiddenException('Credentials taken')
+                }
+            }
+            throw err
+
+        }
+    }
+
+
+    async signin(dto: AuthDto){
+        const user =  await this.prisma.user.findUnique({
+            where : {
+                
+            }
+        })
+        return {message : "I am signed in"}
+    }
+}
