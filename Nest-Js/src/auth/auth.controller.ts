7b4cd974fe3280eb80ea8f96f1e49f584a5de9d2
pratikgirikonvergedotai <pratik.giri@konverge.ai>
2022-04-04 01:35:36 +0530

    nest-js

--- Nest-Js/src/auth/auth.controller.ts
+++ Nest-Js/src/auth/auth.controller.ts
@@ -1,22 +1,26 @@
-import { Body, Controller, Post} from "@nestjs/common";
-import { AuthService } from "./auth.service";
-import { AuthDto } from "./dto";
+import {
+  Body,
+  Controller,
+  HttpCode,
+  HttpStatus,
+  Post,
+  Req,
+} from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { AuthDto } from './dto';
 
 @Controller('auth')
-export class AuthController{
-    constructor(private authService: AuthService){
-        // dependency injection completed   
-    }
+export class AuthController {
+  constructor(private authService: AuthService) {}
 
-    // /auth/signup
-    @Post('signup')
-    signup(@Body()  dto: AuthDto){
-        return this.authService.signup(dto)
-    }
+  @Post('signup')
+  signup(@Body() dto: AuthDto) {
+    return this.authService.signup(dto);
+  }
 
-    // /auth/signin
-    @Post('signin')
-    signin(@Body()  dto: AuthDto){
-        return this.authService.signin(dto)
-    }
+  @HttpCode(HttpStatus.OK)
+  @Post('signin')
+  signin(@Body() dto: AuthDto) {
+    return this.authService.signin(dto);
+  }
 }
